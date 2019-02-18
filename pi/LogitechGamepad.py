51bcebeece203a56537c18a4a95f12552a66a21d
KDragonM <kennethmitra@gmail.com>
2019-02-18 00:46:35 -0600

    Added LogitechGamepad.py to inherit from Controller.py

---
+++ pi/LogitechGamepad.py
@@ -0,0 +1,18 @@
+from Controller import Controller
+import pygame
+# See https://stackoverflow.com/questions/19203819/reading-joystick-values-with-python
+# and https://theraspberryblonde.wordpress.com/2016/06/29/ps3-joystick-control-with-pygame/
+# on how to use pygame to read joystick/gamepad inputs
+
+class LogitechGamepad(Controller):
+    def __init__(self,name="Logitech Gamepad"):
+        Controller.__init__(name)
+
+    def getStatus(self):
+        pass
+
+    def startController(self):
+        """
+        Starts a thread that continuously reads gamepad and updates Controller variables
+        """
+        pass
--- pi/USBController.py
+++
@@ -1,6 +0,0 @@
-import Controller
-class USBController(Controller):
-    def __init__(self):
-        pass
-    def getStatus(self):
-        pass
