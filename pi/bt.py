65560574a637ec545f983eaec6d2984a10f88f06
Takuma <tfujiwara2017@gmail.com>
2019-04-05 16:59:17 -0500

    debugged switch function

--- pi/bt.py
+++ pi/bt.py
@@ -6,6 +6,17 @@
 # Taken from: https://people.csail.mit.edu/albert/bluez-intro/c212.html
 
 import bluetooth
+from ast import literal_eval
+
+def get_type(input_data):
+    """
+    returns data type of received data
+    """
+    try:
+        return type(literal_eval(input_data))
+    except (ValueError, SyntaxError):
+        # A string, so return str
+        return str
 
 def handleRight():
     #do stuff
@@ -31,6 +42,9 @@ def handleStop():
     #do stuff
     print("stop handled")
     return
+
+def handleQuit():
+    return
  
 handler = {
         #all data has letter "b" before string and '' around sent string 
@@ -38,31 +52,48 @@ handler = {
         "'b'left": handleLeft,
         "'b'forward": handleForward,
         "'b'back": handleBack,
-        "'b'stop'": handleStop
+        "'b'quit": handleQuit,
+        "'b'stop": handleStop
+        
 }
  
  
 def handle_input(argument):
-    # Get the function from handler dictionary
-    func = handler.get(argument, "nothing")
+    
+    myType = get_type(argument)
+    if(myType == type("a string")):
+        # Get the function from handler dictionary
+        func = handler.get(str(argument), "nothing")
+    elif(myType == type(1)):
+        #handle speed change
+        print("speed is an integer of %d", argument)
+    elif(myType == type(1.0)):
+        #handle speed change
+        print("handled the following:")
+        print(argument)
+    else:
+        print("handled the following:")
+        print(argument)
     # Execute the function
     return func()
 
 def receiveMessages():
   server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-  
+  print(bluetooth.RFCOMM)
   port = 1
   server_sock.bind(("",port))
   server_sock.listen(1)
   
   client_sock,address = server_sock.accept()
+  isReceiving = True
   print ("Accepted connection from " + str(address))
-  while(1): 
+  while(isReceiving): 
       data = client_sock.recv(1024)
       print ("received [%s]" % data)
-      
-      handle_input(str(data))
-      
+      returnData = handle_input(data)
+      if(returnData == False):
+          isReceiving = False
+
   client_sock.close()
   server_sock.close()
   
