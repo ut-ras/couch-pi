89347137fb324eb928baf4d66110d0cc4e0f07b1
veles <velesmyth@protonmail.com>
2019-10-11 17:58:38 -0500

    starting app v2

---
+++ android_v2/app/src/main/java/projects/demobots/bluetoothmodule_v2/MainActivity.java
@@ -0,0 +1,21 @@
+package projects.demobots.bluetoothmodule_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        setContentView(R.layout.activity_main);
+    }
+}
